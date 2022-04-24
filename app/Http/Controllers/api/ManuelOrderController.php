481f1e6b9b863651535b3a7ac3b3daae890f3047
Rashad Alakbarov <rashadalalakbarov@gmail.com>
2022-04-24 19:38:17 +0400

    update ManuelOrderontroller for api

--- app/Http/Controllers/api/ManuelOrderController.php
+++ app/Http/Controllers/api/ManuelOrderController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\CargoCompany;
+
 class ManuelOrderController extends Controller
 {
     public function index(){
+        $cargo_company=CargoCompany::orderBy('created_at','desc')->get();
+        
         return response()->json([
             'status' => 1, 
+            'cargo_company' => $cargo_company,
         ], 200);
     }
 }
