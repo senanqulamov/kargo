942ccd893c3b534943f86324c9fb6e209bceb5df
ayazibrahimov <ayaz.ibrahimov.1998@mail.ru>
2022-06-10 10:53:17 +0400

    new-change

--- front_files/js/nav.js
+++ front_files/js/nav.js
@@ -6,21 +6,71 @@ const lineCenter = document.querySelector(".fnav__center")
 const lineBottom = document.querySelector(".fnav__bottom")
 const over = document.querySelector(".over")
 const body = document.querySelector('body')
+const overlayActive = document.querySelector(".overlay__active")
+const navigationCancel = document.querySelector('.navigation__cancel')
+
+const navigationBox = document.querySelectorAll(".navigation__box")
 
 
 burgerLines.addEventListener('click', function (e) {
     e.preventDefault()
 
     burgerMenu.classList.toggle("burger__menu-visible")
-    burgerMenu.classList.toggle("visible")
-   
 
-
-    lineTop.classList.toggle('burger__line-1')
-    lineCenter.classList.toggle('burger__line-2')
-    lineBottom.classList.toggle('burger__line-3')
+    overlayActive.classList.add("visible")
+    body.classList.add('overflowY')
 
 })
+
+
+
+document.querySelector(".overlay").addEventListener('click', function () {
+    overlayActive.classList.remove("visible")
+    burgerMenu.classList.remove("burger__menu-visible")
+    body.classList.remove('overflowY')
+})
+
+
+
+
+navigationCancel.addEventListener('click', function () {
+    overlayActive.classList.remove("visible")
+    burgerMenu.classList.remove("burger__menu-visible")
+    body.classList.remove('overflowY')
+})
+
+
+
+
+navigationBox.forEach((box)=>{
+    
+
+    box.addEventListener('click',function(){
+      
+        navigationBox.forEach((tabs)=>{
+            tabs.classList.remove('isActive')
+        })
+
+        box.classList.add('isActive')
+
+    })
+
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
